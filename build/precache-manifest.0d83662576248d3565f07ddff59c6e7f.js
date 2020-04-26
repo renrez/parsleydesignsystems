b5202acab0058c01b18516bde752917476d7bc07
renrez <renrez@renrez.com>
2020-04-26 10:21:17 +1000

    build folder added

---
+++ build/precache-manifest.0d83662576248d3565f07ddff59c6e7f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de97c18338586a2f4dc3e95a2629326f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e1911ff4317e7e0eeb",
+    "url": "/static/css/main.9c309d42.chunk.css"
+  },
+  {
+    "revision": "d2e3adb4867f39bb8165",
+    "url": "/static/js/2.4daa0014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4daa0014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e1911ff4317e7e0eeb",
+    "url": "/static/js/main.fc5af013.chunk.js"
+  },
+  {
+    "revision": "049bf9c2227a3e3003c7",
+    "url": "/static/js/runtime-main.4bb43bcd.js"
+  },
+  {
+    "revision": "334a90a03b625815eea63df012d4213f",
+    "url": "/static/media/adam-zerner.334a90a0.jpg"
+  },
+  {
+    "revision": "f0691f2a9ce71aa4f9a8d701878cceed",
+    "url": "/static/media/beautiful-products.f0691f2a.jpg"
+  },
+  {
+    "revision": "84f46b6806d74e364eefa3f8b42b021e",
+    "url": "/static/media/bg-how-much-does-it-cost.84f46b68.jpg"
+  },
+  {
+    "revision": "6eb019dd926f395e776d3209a0c78ae6",
+    "url": "/static/media/bg-overview.6eb019dd.jpg"
+  },
+  {
+    "revision": "f3d7a400855431eefc9ff78af2918510",
+    "url": "/static/media/bg-the-team.f3d7a400.jpg"
+  },
+  {
+    "revision": "3a267c03358ed1dbb0180fa69ba71add",
+    "url": "/static/media/consistent-experiences.3a267c03.jpg"
+  },
+  {
+    "revision": "1f364c53277592df17dcf160b72b3dad",
+    "url": "/static/media/department-of-education.1f364c53.png"
+  },
+  {
+    "revision": "029ae5e35ade365f41d11331e0e4691f",
+    "url": "/static/media/mike-hall.029ae5e3.jpg"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/static/media/open-sans-v17-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/static/media/open-sans-v17-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "e5111caba5b811a73d995786db3c61ea",
+    "url": "/static/media/open-sans-v17-latin-700.e5111cab.ttf"
+  },
+  {
+    "revision": "049a929c5d81988b3ae6d2f985ca7aa5",
+    "url": "/static/media/open-sans-v17-latin-regular.049a929c.ttf"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  },
+  {
+    "revision": "7e2813bb16f3a601e2b4774abdc3deb9",
+    "url": "/static/media/parsley-browser-l.7e2813bb.png"
+  },
+  {
+    "revision": "d3c604cd4ea434406ff2e0c88d9509d0",
+    "url": "/static/media/parsley-browser-m.d3c604cd.png"
+  },
+  {
+    "revision": "72920f80154433a4fe08a20fa42a9c70",
+    "url": "/static/media/parsley-browser-s.72920f80.png"
+  },
+  {
+    "revision": "b7e2d0b2ec81fcc74f614565b2929aa9",
+    "url": "/static/media/parsley-in-action-l.b7e2d0b2.png"
+  },
+  {
+    "revision": "61c74579ede003122dd179db921dbdf6",
+    "url": "/static/media/parsley-in-action-m.61c74579.png"
+  },
+  {
+    "revision": "6d6d02566c3c39a63d52cdf8697e6d6e",
+    "url": "/static/media/parsley-in-action-s.6d6d0256.png"
+  },
+  {
+    "revision": "060493c80e6f4cbd446d1c53ee8ef947",
+    "url": "/static/media/sustainable-code.060493c8.jpg"
+  }
+]);
